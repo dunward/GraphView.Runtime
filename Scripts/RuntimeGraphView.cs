14bb08e6b1d5d276e13637726a18a3d0518500a8
Elik Song <elik.song@icloud.com>
2024-10-11 01:03:47 +0900

    Add runtime graph viewer zoom logic

--- Scripts/RuntimeGraphView.cs
+++ Scripts/RuntimeGraphView.cs
@@ -15,6 +15,10 @@ namespace Dunward.GraphView.Runtime
         private Vector2 lastMousePosition;
         private Vector2 lastViewerPosition;
 
+        private float minZoom = 0.25f;
+        private float maxZoom = 1f;
+        private float zoomStep = 0.15f;
+
         public void Update()
         {
             if (Input.GetMouseButtonDown(2))
@@ -33,6 +37,15 @@ namespace Dunward.GraphView.Runtime
             if (isDragViewer)
             {
                 viewer.transform.localPosition = lastViewerPosition + (Vector2)Input.mousePosition - lastMousePosition;
+            }
+
+            // Scroll condition
+            if (Input.mouseScrollDelta.y != 0 && !isDragViewer)
+            {
+                var scale = viewer.transform.localScale.x;
+                scale += Input.mouseScrollDelta.y > 0 ? zoomStep : -zoomStep;
+                scale = Mathf.Clamp(scale, minZoom, maxZoom);
+                viewer.transform.localScale = new Vector3(scale, scale, 1);
             }
         }
     }
