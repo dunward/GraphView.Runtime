dc48737a14d59f490650950c9a23963b18fb1cd4
Elik Song <elik.song@icloud.com>
2024-10-19 20:58:15 +0900

    Add UGUI culling test

---
+++ Scripts/CullingTest.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+
+using UnityEngine;
+
+public class CullingTest : MonoBehaviour
+{
+    private RectTransform uiElement;
+    private Canvas canvas;
+    public RectTransform viewPort;
+
+    private void Awake()
+    {
+        uiElement = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        StartCoroutine(Run());
+    }
+
+    private IEnumerator Run()
+    {
+        while (true)
+        {
+            if (CalculateVisibility())
+            {
+                Debug.Log("UI Element is visible");
+                canvas.enabled = true;
+            }
+            else
+            {
+                Debug.Log("UI Element is not visible");
+                canvas.enabled = false;
+            }
+            yield return null;
+        }
+    }
+
+    private bool CalculateVisibility()
+    {
+        if (uiElement == null || viewPort == null)
+            return false;
+
+        var rect = GetWorldRect(uiElement);
+        if (rect.width <= 0 || rect.height <= 0)
+            return false;
+
+        var viewport = GetWorldRect(viewPort);
+        if (viewport.width <= 0 || viewport.height <= 0)
+            return false;
+
+        var visible = rect.Overlaps(viewport);
+        return visible;
+    }
+
+    private Rect GetWorldRect(RectTransform rt)
+    {
+        Vector3[] corners = new Vector3[4];
+        rt.GetWorldCorners(corners);
+        
+        Vector3 bottomLeft = corners[0];
+        Vector3 topRight = corners[2];
+        
+        return new Rect(bottomLeft, topRight - bottomLeft);
+    }
+}
