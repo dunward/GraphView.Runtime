39e4b9043a501cad67a3ae2d019ddac13b0305ec
Elik Song <elik.song@icloud.com>
2024-10-30 22:12:23 +0900

    Add graph input handler skeleton

---
+++ Scripts/GraphInputHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Dunward.GraphView.Runtime
+{
+    public class GraphInputHandler : MonoBehaviour
+    {
+        
+    }
+}
